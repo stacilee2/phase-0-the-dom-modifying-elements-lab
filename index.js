37240199e23fa966044d26ab98986ac64699bb37
Staci Rosales <stacileep2@gmail.com>
2022-10-15 12:55:42 -0500

    commit tree 233b9351b7ad29c8f35f101ea894e0d88607addd

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+const idName = newHeader.id = 'victory'
+const updateHeader = newHeader.textContent = "Staci is the champion"
